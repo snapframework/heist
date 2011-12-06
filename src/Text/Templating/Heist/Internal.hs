7f4e7bec7cff1d6a8f64c682d99fea2ec0382e96
Mighty Byte <mightybyte@gmail.com>
2011-12-06 15:04:45 -0500

    Fixed issue 13, and added test case to detect the problem.

--- src/Text/Templating/Heist/Internal.hs
+++ src/Text/Templating/Heist/Internal.hs
@@ -374,7 +374,8 @@ parseAtt bs = do
     return $ T.concat chunks
   where
     cvt (Literal x) = return x
-    cvt (Ident x)   = getAttributeSplice x
+    cvt (Ident x)   =
+        localParamNode (const $ X.Element x [] []) $ getAttributeSplice x
 
 
 ------------------------------------------------------------------------------
---
+++ test/templates/attrsubtest2.tpl
@@ -0,0 +1,1 @@
+<bind tag="randName284">asdf</bind><apply template="attrsubtest1"><a href="${randName284}">link</a></apply>
