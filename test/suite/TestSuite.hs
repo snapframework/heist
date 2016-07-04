cfdf3c4db89a55ee7c667ff36ed3cbe1064d058d
Doug Beardsley <mightybyte@gmail.com>
2016-07-04 01:01:47 -0400

    Use cabal test infrastructure

--- test/suite/TestSuite.hs
+++ test/suite/TestSuite.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.Directory
 import Test.Framework (defaultMain, testGroup)
 
 import qualified Heist.Interpreted.Tests
@@ -7,7 +8,10 @@ import qualified Heist.Compiled.Tests
 import qualified Heist.Tests
 
 main :: IO ()
-main = defaultMain tests
+main = do
+    -- Need to change directory after we switched to cabal test infra
+    setCurrentDirectory "test"
+    defaultMain tests
   where tests = [ testGroup "Heist.Interpreted.Tests"
                             Heist.Interpreted.Tests.tests
                 , testGroup "Heist.Compiled.Tests"
