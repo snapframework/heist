f1b3559090e9c75ce1d46969085fe4f918b2fa86
Doug Beardsley <mightybyte@gmail.com>
2016-07-04 15:52:21 -0400

    Fix loadTest test

--- test/suite/Heist/Interpreted/Tests.hs
+++ test/suite/Heist/Interpreted/Tests.hs
@@ -141,7 +141,7 @@ loadTest = do
     ets <- loadIO "templates" mempty mempty mempty mempty
     either (error "Error loading templates")
            (\ts -> do let tm = _templateMap ts
-                      H.assertEqual "loadTest size" 40 $ Map.size tm
+                      H.assertEqual "loadTest size" 41 $ Map.size tm
            ) ets
 
 
